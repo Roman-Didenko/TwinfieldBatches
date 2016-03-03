a298adc820454922091a06a164d8b809970250eb
Roman Didenko <roman_didenko@epam.com>
2016-03-03 16:02:13 +0200

    extended run-all

--- run-all.cmd
+++ run-all.cmd
@@ -7,6 +7,8 @@ find ": error " log.txt 1>nul && goto error
 call tf-build-all.cmd 2>&1 > log.txt
 find "Error:" log.txt 1>nul && goto error
 find ": error " log.txt 1>nul && goto error
+find "errors during the processing" log.txt 1>nul && goto error
+find " error:" log.txt 1>nul && goto error
 
 rd C:\Migrations /S /Q
 call tf-execute-db-migrations.cmd 2>&1 > log.txt
